c8bfc2901e53a63b8cf67fff20278d9517ebf2d6
Jason Ma <jasonma062019@gmail.com>
2025-03-05 23:01:31 -0800

    Added more UMPIRE notes for problem #1

--- Breakout Problems Session 5/AdvancedProblemSet1V1.py
+++ Breakout Problems Session 5/AdvancedProblemSet1V1.py
@@ -8,7 +8,10 @@ def arrange_guest_arrival_order(arrival_pattern):
     P:
         Theory #1: What if I add the digits from 1 to 9 onto the stack, if it is an 'I', get from the bottom of the stack,
         and if it is 'D', pop from the top of the stack
-    I: Iterate through the pattern and use the digits from 1 to 9 on a stack
+    I: 1. Add each value onto the stack
+       2. If the letter is 'I', pop each value from the stack and add it to guess_order.
+       3. If the letter is 'D', nothing happens besides adding the value to to the stack
+       4. If i reached the end, pop the rest of the values from the stack and add it to guest_order.
     R:
     E:
     """
